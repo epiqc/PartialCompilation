8d47c3a665f4235cb5b561f294f7db84eea864c3
Thomas Propson <tcpropson@gmail.com>
2019-01-23 19:15:55 -0600

    uccsd unitary and theta unitaries implemented

--- vqe/uccsd_qoc.py
+++ vqe/uccsd_qoc.py
@@ -12,9 +12,11 @@ from IPython import display
 import scipy.linalg as la
 from quantum_optimal_control.helper_functions.grape_functions import *
 from quantum_optimal_control.main_grape.grape import Grape
-# FIXME: This module is imported relatively and should be imported absolutley
-# once a setup.py is added to vqe
-from util import kron_many
+from quantum_optimal_control.core.hamiltonian import (get_H0,
+                                                      get_full_states_concerned_list,
+                                                      get_H0, get_Hops_and_Hnames)
+
+from uccsd_unitary import get_uccsd_unitary
 
 # Parse CLI and define constants.
 # TODO: add "states" parameter and adapt file to handle qudits.
@@ -22,19 +24,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--iterations", type=int, default=1000,
                     help="The maximum number of iterations to perform "
                          "optimization for.")
-parser.add_argument("-o", "--ops", type=str, default="HHHH",
-                    help="The four single qubit operators of the circuit.")
-parser.add_argument("-q", "--qubits", type=int, default=4,
-                    help="The number of qubits to build the circuit for.")
-parser.add_argument("-t", "--theta", type=int, default=0,
-                    help="The angle for the Rz gate in the uccsd circuit.")
 args = vars(parser.parse_args())
 
+# TODO: Handle uccsd ansatz for arbitrary state number and qubit count.
+# We will want to make a module for generating arbitrary UCCSD 
+# circuits via qiskit.
+NUM_QUBITS = 4
 NUM_STATES = 2
+# TODO: What connected qubit pairs do we want?
+# An empty list will return a zero matrix for the dirft hamiltonian.
+CONNECTED_QUBIT_PAIRS = []
 MAX_ITERATIONS = args["iterations"]
-SQOPS = args["ops"]
-NUM_QUBITS = args["qubits"]
-THETA = args["theta"]
 
 DATA_PATH = '../out/'
 FILE_NAME = 'uccsd_qoc'
@@ -43,72 +43,31 @@ FILE_NAME = 'uccsd_qoc'
 TOTAL_TIME = 10.0
 STEPS = 1000
 
-# TODO: get drift hamiltonian from Pranav's hamiltonian file in q_o_c/core/hamiltonian.py
-# Define H0 (drift hamiltonian)
-ens = lambda freq, alpha: [2*np.pi*ii*(freq - 0.5*(ii-1)*alpha) 
-                               for ii in np.arange(NUM_STATES)]
+# Define target unitary
+theta = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]
+U = get_uccsd_unitary(theta)
 
-# Frequencies in GHz, "ens" may refer to "eigens"
-alpha_list = [0.255 for i in range(NUM_QUBITS)]
-freq_ge_list = [3.9] + [3.5 for i in range(NUM_QUBITS - 1)]
-H0_list = [np.diag(ens(freq_ge_list[i], alpha_list[i])) for i in range(NUM_QUBITS)]
+# Define drift hamiltonian
+H0 = get_H0(NUM_QUBITS, NUM_STATES, CONNECTED_QUBIT_PAIRS)
 
-G = 2*np.pi*0.1
-
-Q_x   = np.diag(np.sqrt(np.arange(1,NUM_STATES)),1)+np.diag(np.sqrt(np.arange(1,NUM_STATES)),-1)
-Q_y   = (0+1j)*(np.diag(np.sqrt(np.arange(1,NUM_STATES)),1)-np.diag(np.sqrt(np.arange(1,NUM_STATES)),-1))
-Q_z   = np.diag(np.arange(0,NUM_STATES))
-I = np.identity(NUM_STATES)
-
-# To obtain the drift hamiltonian, by symmetry with examples, we take the sum of 
-# the matrices which are the (NUM_QUBITS) permutations of taking the kronecker
-# product of a gate from H0_list with (NUM_QUBITS - 1) identity matrices, plus a constant.
-base = [I for i in range(NUM_QUBITS - 1)]
-H0_permutations = []
-for i in range(NUM_QUBITS):
-    H0_gates = base[:]
-    H0_gates.insert(i, H0_list[i])
-    H0_permutations.append(kron_many(*H0_gates))
-
-H0 = reduce(lambda x, y: x + y, H0_permutations) + G * kron_many(*[Q_x for i in range(NUM_QUBITS)])
+# Define controls
+Hops, Hnames = get_Hops_and_Hnames(NUM_QUBITS, NUM_STATES)
 
 # Define concerned states (starting states)
-psi0 = [0,1,NUM_STATES,NUM_STATES+1] #[gg,ge,eg,ee]
+psi0 = get_full_states_concerned_list(NUM_QUBITS, NUM_STATES)
 
 # Define states to include in the drawing of occupation
 states_draw_list = [0, 1, NUM_STATES, NUM_STATES+1]
 states_draw_names = ['00','01','10','11']
 
-# Define Target Uniatry 'U'.
-# TODO: Implement Qiskit UCCSD Unitary.
-U = np.matrix([[0]])
-
-# Define controls
-# We want, by symmetry with examples, Q_xi for i in {1..NUM_QUBITS} and Q_zn 
-# where n = NUM_QUBITS. Q_xi is the (NUM_QUBITS) permutations of (NUM_QUBITS - 1)
-# identity matrices with Q_x in the ith position.
-Hops = []
-Hnames = []
-for i in range(NUM_QUBITS):
-    gates = base[:]
-    gates.insert(i, Q_x)
-    Hops.append(kron_many(*gates))
-    Hnames.append('x' + str(i))
-gates = base[:]
-gates.append(Q_z)
-Hops.append(kron_many(*gates))
-Hnames.append('z' + str(NUM_QUBITS))
-ops_max_amp = [np.pi for _ in range(NUM_QUBITS + 1)]
-
 # Define convergence parameters
-DECAY = MAX_ITERATIONS/2
-convergence = {'rate':0.01, 'update_step':10, 'max_iterations':MAX_ITERATIONS,\
-               'conv_target':1e-3, 'learning_rate_decay':DECAY}
+decay = MAX_ITERATIONS/2
+
+convergence = {'rate':0.01, 'update_step':10, 'max_iterations':MAX_ITERATIONS, \
+               'conv_target':1e-3, 'learning_rate_decay':decay}
 
 # Define reg coeffs
-
 states_forbidden_list = []
-
 for ii in range(NUM_STATES):
     forbid_state = (NUM_STATES-1)*NUM_STATES+ii
     if not forbid_state in states_forbidden_list:
@@ -127,14 +86,15 @@ for ii in range(NUM_STATES):
     forbid_state = ii*NUM_STATES + (NUM_STATES-2)
     if not forbid_state in states_forbidden_list:
         states_forbidden_list.append(forbid_state)
-        
-# print ("states_forbidden_list", states_forbidden_list)
 
-## nothing
+# Define penalties
+ops_max_amp = [np.pi for _ in range(NUM_QUBITS + 1)]
+        
+# nothing
 #reg_coeffs = {'envelope' : 0.0, 'dwdt':0.0,'d2wdt2':0.0,'forbidden':0.0,
 #             'states_forbidden_list': states_forbidden_list,'forbid_dressed':False}
 
-## forbid
+# forbid
 #reg_coeffs = {'envelope' : 0.0, 'dwdt':0.0,'d2wdt2':0.0, 'forbidden':50.0,
 #              'states_forbidden_list': states_forbidden_list,'forbid_dressed':False}
 
@@ -143,11 +103,12 @@ reg_coeffs = {'amplitude':0.01,'dwdt':0.00007,'d2wdt2':0.0,
               'forbidden_coeff_list':[10] * len(states_forbidden_list),
               'states_forbidden_list': states_forbidden_list,'forbid_dressed':False}
 
-
 if __name__ == "__main__":
-#     uks,U_f =Grape(H0, Hops, Hnames, U, TOTAL_TIME, STEPS, psi0, convergence=convergence, 
-#                method = 'L-BFGS-B', draw = [states_draw_list,states_draw_names] ,
-#                maxA = ops_max_amp, use_gpu=False, sparse_H = False, reg_coeffs=reg_coeffs, 
-#                unitary_error = 1e-08, save_plots=True, file_name=FILE_NAME, 
-#                Taylor_terms = [20,0], data_path = DATA_PATH)
-    pass
+    uks,U_f = Grape(H0, Hops, Hnames, U, TOTAL_TIME, STEPS, psi0, 
+                    convergence = convergence, method = 'L-BFGS-B', 
+                    draw = [states_draw_list, states_draw_names] , 
+                    maxA = ops_max_amp, use_gpu = False, sparse_H = False,
+                    reg_coeffs = reg_coeffs, unitary_error = 1e-08, 
+                    save_plots = True, file_name = FILE_NAME, 
+                    Taylor_terms = [20,0], data_path = DATA_PATH)
+
--- vqe/uccsd_unitary.py
+++ vqe/uccsd_unitary.py
@@ -1,11 +1,17 @@
-""" Build the UCCSD circuit unitary
-based on example from: https://github.com/Qiskit/qiskit-tutorials/blob/master/qiskit/aqua/chemistry/programmatic_approach.ipynb
+"""
+uccsd_unitary.py - A module for generating unitary matrices
+                   that represent UCCSD operator circuits.
 """
 
-# import common packages
+from math import pi
+
 import numpy as np
 
-from qiskit import Aer
+#lib from Qiskit Terra
+from qiskit import BasicAer, QuantumCircuit, ClassicalRegister, QuantumRegister, execute
+from qiskit.extensions.standard.u1 import U1Gate
+from qiskit.extensions.standard.u2 import U2Gate
+from qiskit.extensions.standard.u3 import U3Gate
 
 # lib from Qiskit Aqua
 from qiskit_aqua import Operator, QuantumInstance
@@ -17,6 +23,18 @@ from qiskit_chemistry import FermionicOperator
 from qiskit_chemistry.drivers import PySCFDriver, UnitsType
 from qiskit_chemistry.aqua_extensions.components.variational_forms import UCCSD
 from qiskit_chemistry.aqua_extensions.components.initial_states import HartreeFock
+
+backend = BasicAer.get_backend('unitary_simulator')
+
+### BUILD THE UCCSD VARIATIONAL FORM ###
+
+# This section follows:
+# https://github.com/Qiskit/qiskit-terra/blob/master/qiskit/circuit/instruction.py
+
+# TODO: This section is not wrapped in a funciton because we
+# want the variational form to be precompiled in the python binary.
+# When we begin to work with different variational forms we should
+# then modulate it.
 
 # using driver to get fermionic Hamiltonian
 # PySCF example
@@ -35,9 +53,9 @@ nuclear_repulsion_energy = molecule.nuclear_repulsion_energy
 
 num_particles = molecule.num_alpha + molecule.num_beta
 num_spin_orbitals = molecule.num_orbitals * 2
-print("HF energy: {}".format(molecule.hf_energy - molecule.nuclear_repulsion_energy))
-print("# of electrons: {}".format(num_particles))
-print("# of spin orbitals: {}".format(num_spin_orbitals))
+#print("HF energy: {}".format(molecule.hf_energy - molecule.nuclear_repulsion_energy))
+#print("# of electrons: {}".format(num_particles))
+#print("# of spin orbitals: {}".format(num_spin_orbitals))
 
 # prepare full idx of freeze_list and remove_list
 # convert all negative idx to positive
@@ -66,16 +84,14 @@ qubitOp = ferOp.mapping(map_type=map_type, threshold=0.00000001)
 qubitOp = qubitOp.two_qubit_reduced_operator(num_particles) if qubit_reduction else qubitOp
 qubitOp.chop(10**-10)
 
-print(qubitOp.print_operators())
-print(qubitOp)
+#print(qubitOp.print_operators())
+#print(qubitOp)
 
 # Using exact eigensolver to get the smallest eigenvalue
 exact_eigensolver = ExactEigensolver(qubitOp, k=1)
 ret = exact_eigensolver.run()
-print('The computed energy is: {:.12f}'.format(ret['eigvals'][0].real))
-print('The total ground state energy is: {:.12f}'.format(ret['eigvals'][0].real + energy_shift + nuclear_repulsion_energy))
-
-backend = Aer.get_backend('statevector_simulator')
+#print('The computed energy is: {:.12f}'.format(ret['eigvals'][0].real))
+#print('The total ground state energy is: {:.12f}'.format(ret['eigvals'][0].real + energy_shift + nuclear_repulsion_energy))
 
 # setup COBYLA optimizer
 max_eval = 200
@@ -92,9 +108,92 @@ var_form = UCCSD(qubitOp.num_qubits, depth=1,
                    initial_state=HF_state, qubit_mapping=map_type, 
                    two_qubit_reduction=qubit_reduction, num_time_slices=1)
 
-# FIXME: What are the necessary parameters?
-# refer to:
-# https://github.com/Qiskit/qiskit-chemistry/blob/master/qiskit_chemistry/aqua_extensions/components/variational_forms/uccsd.py
 
-#circuit = var_form.construct_circuit()
-#uccsd_circuit.draw()
+### BUILD CIRCUTS AND UNITARIES ###
+
+def get_uccsd_unitary(theta_vector):
+    """Return the unitary matrix for the entire UCCSD circuit.
+       
+       theta_vector :: array - arguments for the vqe ansatz
+    """
+    circuit = var_form.construct_circuit(theta_vector)
+    job = execute(circuit, backend)
+    return job.result().get_unitary(circuit, decimals=10)
+
+# The four gates that comprise the UCCSD circuit are CX, U1, U2, and U3.
+# The latter three gates are dependent on the theta vector.
+def _is_theta_dependent(gate):
+    """Return ture if a gate is dependent on the theta vector,
+    false otherwise.
+    
+        gate :: qiskit.QuantumGate - an arbitrary quantum gate
+    """
+    return (isinstance(gate, U1Gate) 
+            or isinstance(gate, U2Gate) 
+            or isinstance(gate, U3Gate))
+
+# https://github.com/Qiskit/qiskit-terra/blob/master/qiskit/circuit/instruction.py
+# The syntax will change from `gate.param` to `gate.params` in a future release of
+# qiskit terra
+def _append_theta_dependent_gate(circuit, register, gate):
+    """Take a U1, U2, or U3 gate object and append it to a circuit.
+    
+        circuit :: qiskit.QuantumCircuit - the circuit to apply the gate to
+        register :: qiskit.QuantumRegister - the register that defines the circuit
+        gate :: qiskit.QuantumGate - one of the following gates: U1, U2, or U3 
+    """
+    # Get the qubit index that the gate should be applied to.
+    qubit = gate.qargs[0][1]
+
+    if isinstance(gate, U1Gate):
+        constructor = circuit.u1
+    elif isinstance(gate, U2Gate):
+        constructor = circuit.u2
+    else:
+        constructor = circuit.u3
+
+    constructor(*gate.param, register[qubit])
+
+    return
+
+def get_theta_unitaries(theta_vector):
+    """Return a list of unitaries that comprise the continuous spans
+       of gates in the UCCSD circuit that depend on the values of 
+       the theta_vector.
+       
+       theta_vector :: array - arguments for the vqe ansatz
+    """
+
+    full_circuit = get_uccsd_unitary(theta_vector)
+    gates = full_circuit.data
+    gate_count = len(gates)
+
+    # Walk the list of gates and make a new quantum circuit
+    # for every continuous span of theta dependent gates.
+    # Store the unitary of the partial circuit in theta_unitaries.
+    i = 0
+    theta_unitaries = list()
+    while(i < gate_count):
+        # Construct a 4 qubit circuit.
+        register = QuantumRegister(4)
+        circuit = QuantumCircuit(register)
+
+        # Add the continuous span of gates
+        # to the partial circuit.
+        while(i < gate_count and _is_theta_dependent(gates[i])):
+            _append_theta_dependent_gate(circuit, register, gates[i])
+            i += 1
+
+        # If there were theta depedent gates found 
+        # in this stretch, grab their circuit.
+        # Then, turn the circuit into a unitary.
+        if (len(circuit.data) > 0):
+            job = execute(circuit, backend)
+            unitary = job.result().get_unitary(circuit, decimals=6)
+            theta_unitaries.append((circuit, unitary))
+        else:
+            i += 1
+    
+    return theta_unitaries
+
+
