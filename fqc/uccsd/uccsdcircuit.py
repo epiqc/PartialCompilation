2f080ecd7de2ed96d053fd801b1956c104556122
Pranav Gokhale <pranavgokhale@uchicago.edu>
2019-02-18 00:59:30 -0600

    Add UCCSD benchmarks for H2O, NaH, H2, BeH2

--- fqc/uccsd/uccsdcircuit.py
+++ fqc/uccsd/uccsdcircuit.py
@@ -27,20 +27,52 @@ backend = BasicAer.get_backend('unitary_simulator')
 ### BUILD CIRCUTS AND UNITARIES ###
 
 
-def get_uccsd_circuit(theta_vector, use_basis_gates=False):
+class MoleculeInfo(object):
+    def __init__(self, atomic_string, orbital_reduction, active_occupied=[], active_unoccupied=[]):
+        self.atomic_string = atomic_string
+        self.orbital_reduction = orbital_reduction
+
+        # TODO: what should I pass in for active (un)occupied for non LiH molecules?
+        self.active_occupied = active_occupied
+        self.active_unoccupied = active_unoccupied
+
+
+MOLECULE_TO_INFO = {
+    'LiH': MoleculeInfo('Li .0 .0 .0; H .0 .0 1.6', [-3, -2], [0], [0, 1]),
+
+    # Minimum energy is at 1.3 Angstrom intermolecular distance. [-4, -3] reduction performs well.
+    # github.com/Qiskit/qiskit-tutorials/blob/master/community/aqua/chemistry/beh2_reductions.ipynb
+    'BeH2': MoleculeInfo('H .0 .0 -1.3; Be .0 .0 .0; H .0 .0 1.3', [-4, -3]),
+
+    # Minimum energy is at 1.7/2 Angstrom intermolecular distance.
+    # github.com/Qiskit/qiskit-tutorials/blob/master/community/aqua/chemistry/nah_uccsd.ipynb
+    'NaH': MoleculeInfo('H .0 .0 -0.85; Na .0 .0 0.85', []),
+
+    # Minimum energy is at 0.7/2 Angstrom intermolecular distance.
+    # github.com/Qiskit/qiskit-tutorials/blob/master/community/aqua/chemistry/h2_uccsd.ipynb
+    'H2': MoleculeInfo('H .0 .0 -0.35; H .0 .0 0.35', []),
+
+    # github.com/Qiskit/qiskit-tutorials/blob/master/community/aqua/chemistry/h2o.ipynb
+    'H2O': MoleculeInfo('O 0.0 0.0 0.0; H 0.757 0.586 0.0; H -0.757 0.586 0.0', []),
+    }
+
+
+def get_uccsd_circuit(molecule, theta_vector=None, use_basis_gates=False):
     """Produce the full UCCSD circuit.
     Args:
-    theta_vector :: array - arguments for the vqe ansatz
+    molecule :: string - must be a key of MOLECULE_TO_INFO
+    theta_vector :: array - arguments for the vqe ansatz. If None, will generate random angles.
     use_basis_gates :: bool - Mike and Ike gates if False, Basis gates if True.
        
     Returns:
     circuit :: qiskit.QuantumCircuit - the UCCSD circuit parameterized
                                        by theta_vector
     """
-    driver = PySCFDriver(atom='Li .0 .0 .0; H .0 .0 1.6', basis='sto3g')
+    molecule_info = MOLECULE_TO_INFO[molecule]
+    driver = PySCFDriver(atom=molecule_info.atomic_string, basis='sto3g')
     qmolecule = driver.run()
     hamiltonian = Hamiltonian(qubit_mapping=QubitMappingType.PARITY, two_qubit_reduction=True,
-                              freeze_core=True, orbital_reduction=[-3, -2])
+                              freeze_core=True, orbital_reduction=molecule_info.orbital_reduction)
 
     energy_input = hamiltonian.run(qmolecule)
     qubit_op = energy_input.qubit_op
@@ -53,9 +85,13 @@ def get_uccsd_circuit(theta_vector, use_basis_gates=False):
                            qubit_reduction)
     var_form = UCCSD(qubit_op.num_qubits, depth=1,
                      num_orbitals=num_spin_orbitals, num_particles=num_particles,
-                     active_occupied=[0], active_unoccupied=[0, 1],
+                     active_occupied=molecule_info.active_occupied,
+                     active_unoccupied=molecule_info.active_unoccupied,
                      initial_state=HF_state, qubit_mapping=map_type,
                      two_qubit_reduction=qubit_reduction, num_time_slices=1)
+
+    if theta_vector is None:
+        theta_vector = [np.random.random() * 2 * np.pi for _ in range(var_form._num_parameters)]
 
     return var_form.construct_circuit(theta_vector, use_basis_gates=use_basis_gates)
 
