6aea05df2046c077d2fd3a581462e2fc8b80b2a3
Thomas Propson <tcpropson@gmail.com>
2019-02-25 20:05:18 -0600

    added run note

--- scripts/g2_slices_1551143462/node3.sh
+++ scripts/g2_slices_1551143462/node3.sh
@@ -11,44 +11,44 @@ ANGLE_ARGS="$ASTART 185 $ASTOP 356 $ASTEP 5"
 ANGLE_ARGS1="$ASTART 185 $ASTOP 271 $ASTEP 5"
 ANGLE_ARGS2="$ASTART 275 $ASTOP 356 $ASTEP 5"
 
-screen -AmdS 0 bash
-screen -S 0 -X stuff $"$PREFIX $SSTART 0 $SSTOP 0 $ANGLE_ARGS\r"
+screen -AmdS q0 bash
+screen -S q0 -X stuff $"$PREFIX $SSTART 0 $SSTOP 0 $ANGLE_ARGS\r"
 
-screen -AmdS 1 bash
-screen -S 1 -X stuff $"$PREFIX $SSTART 1 $SSTOP 1 $ANGLE_ARGS\r"
+screen -AmdS q1 bash
+screen -S q1 -X stuff $"$PREFIX $SSTART 1 $SSTOP 1 $ANGLE_ARGS\r"
 
-screen -AmdS 2 bash
-screen -S 2 -X stuff $"$PREFIX $SSTART 2 $SSTOP 2 $ANGLE_ARGS\r"
+screen -AmdS q2 bash
+screen -S q2 -X stuff $"$PREFIX $SSTART 2 $SSTOP 2 $ANGLE_ARGS\r"
 
-screen -AmdS 3 bash
-screen -S 3 -X stuff $"$PREFIX $SSTART 3 $SSTOP 3 $ANGLE_ARGS\r"
+screen -AmdS q3 bash
+screen -S q3 -X stuff $"$PREFIX $SSTART 3 $SSTOP 3 $ANGLE_ARGS\r"
 
-screen -AmdS 4 bash
-screen -S 4 -X stuff $"$PREFIX $SSTART 4 $SSTOP 4 $ANGLE_ARGS\r"
+screen -AmdS q4 bash
+screen -S q4 -X stuff $"$PREFIX $SSTART 4 $SSTOP 4 $ANGLE_ARGS\r"
 
-screen -AmdS 5 bash
-screen -S 5 -X stuff $"$PREFIX $SSTART 5 $SSTOP 5 $ANGLE_ARGS1\r"
+screen -AmdS q5 bash
+screen -S q5 -X stuff $"$PREFIX $SSTART 5 $SSTOP 5 $ANGLE_ARGS1\r"
 
-screen -AmdS 6 bash
-screen -S 6 -X stuff $"$PREFIX $SSTART 5 $SSTOP 5 $ANGLE_ARGS2\r"
+screen -AmdS q6 bash
+screen -S q6 -X stuff $"$PREFIX $SSTART 5 $SSTOP 5 $ANGLE_ARGS2\r"
 
-screen -AmdS 7 bash
-screen -S 7 -X stuff $"$PREFIX $SSTART 6 $SSTOP 6 $ANGLE_ARGS1\r"
+screen -AmdS q7 bash
+screen -S q7 -X stuff $"$PREFIX $SSTART 6 $SSTOP 6 $ANGLE_ARGS1\r"
 
-screen -AmdS 8 bash
-screen -S 8 -X stuff $"$PREFIX $SSTART 6 $SSTOP 6 $ANGLE_ARGS2\r"
+screen -AmdS q8 bash
+screen -S q8 -X stuff $"$PREFIX $SSTART 6 $SSTOP 6 $ANGLE_ARGS2\r"
 
-screen -AmdS 9 bash
-screen -S 9 -X stuff $"$PREFIX $SSTART 7 $SSTOP 7 $ANGLE_ARGS\r"
+screen -AmdS q9 bash
+screen -S q9 -X stuff $"$PREFIX $SSTART 7 $SSTOP 7 $ANGLE_ARGS\r"
 
-screen -AmdS 10 bash
-screen -S 10 -X stuff $"$PREFIX $SSTART 8 $SSTOP 8 $ANGLE_ARGS\r"
+screen -AmdS q10 bash
+screen -S q10 -X stuff $"$PREFIX $SSTART 8 $SSTOP 8 $ANGLE_ARGS\r"
 
-screen -AmdS 11 bash
-screen -S 11 -X stuff $"$PREFIX $SSTART 9 $SSTOP 9 $ANGLE_ARGS\r"
+screen -AmdS q11 bash
+screen -S q11 -X stuff $"$PREFIX $SSTART 9 $SSTOP 9 $ANGLE_ARGS\r"
 
-screen -AmdS 12 bash
-screen -S 12 -X stuff $"$PREFIX $SSTART 10 $SSTOP 10 $ANGLE_ARGS\r"
+screen -AmdS q12 bash
+screen -S q12 -X stuff $"$PREFIX $SSTART 10 $SSTOP 10 $ANGLE_ARGS\r"
 
-screen -AmdS 13 bash
-screen -S 13 -X stuff $"$PREFIX $SSTART 11 $SSTOP 11 $ANGLE_ARGS\r"
+screen -AmdS q13 bash
+screen -S q13 -X stuff $"$PREFIX $SSTART 11 $SSTOP 11 $ANGLE_ARGS\r"
