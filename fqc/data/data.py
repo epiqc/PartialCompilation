c0746884e84b04b65beb335c0453deccc0048a1a
Thomas Propson <tcpropson@gmail.com>
2019-03-29 15:38:35 -0500

    updated s4-s7 hyperparameters

--- fqc/data/data.py
+++ fqc/data/data.py
@@ -3,37 +3,40 @@ data.py - A module for storing experimental results and constants.
 
 Notes:
 - All times are in nanoseconds.
+- All slices are assumed to be those such that slice_granularity = 2,
+  dependence_grouping = True.
 
 """
 
 ### GENERAL ###
 
+
 # See Gate_Times.ipynb for determination of these pulse times.
 GATE_TIMES = {'h': 1.4, 'cx': 3.8, 'rz': 0.4, 'rx': 2.5, 'x': 2.5, 'swap': 7.4}
 
-### UCCSD - LiH ###
 
-# Hyperparemeters for Grape on ADAM indexed by slice.
-# Determined by /project/ftchong/qoc/thomas/uccsd_slice_hpo/lih/.
-UCCSD_LIH_SLICE_HYPERPARAMETERS = [
-    {'lr': 2e-2, 'decay': 1e3},
-    {'lr': 2e-2, 'decay': 1e3},
-    {'lr': 2e-2, 'decay': 1e3},
-    {'lr': 2e-2, 'decay': 1e3},
-    {'lr': 2e-1, 'decay': 1e3},
-    {'lr': 2e-1, 'decay': 1e3},
-    {'lr': 2e-1, 'decay': 1e3},
-    {'lr': 2e-1, 'decay': 1e3},
-]
+### UCCSD - LiH ###
 
 # A randomly generated theta vector to use for experimental consistency.
 UCCSD_LIH_THETA = [0.86203, 3.8037, 3.3223, 1.766, 1.0846, 1.4558, 1.0592,
                    0.091974]
 
+# Hyperparemeters for Grape on ADAM indexed by slice.
+# Determined by /project/ftchong/qoc/thomas/hpo/uccsd_lih_slice_hpo.
+UCCSD_LIH_SLICE_HYPERPARAMETERS = [
+    {'lr': 2e-2, 'decay': 1e3}, #s0
+    {'lr': 2e-2, 'decay': 1e3}, #s1
+    {'lr': 2e-2, 'decay': 1e3}, #s2
+    {'lr': 2e-2, 'decay': 1e3}, #s3
+    {'lr': 3.2e-3, 'decay': 2e4}, #s4
+    {'lr': 6.2e-3, 'decay': 5e3}, #s5
+    {'lr': 1.2e-2, 'decay': 3e3}, #s6
+    {'lr': 1.1e-3, 'decay': 3e3}, #s7
+]
+
 # The pulse times for each of the uccsd lih slices. These times correspond
 # to those found by binary search at
-# /project/ftchong/qoc/thomas/uccsd_slice_time/lih_v1-2/ for slice 0-3
-# /project/ftchon/qoc/thomas/uccsd_slice_time/lih/ for slice 4-7
+# /project/ftchong/qoc/thomas/time/uccsd_lih_slice_time
 # plus the maximum pulse time for an RZ gate times the number of RZ gates
 # in the slice. These slices are the granularity 2, depedence grouping = True
 # slices.
@@ -42,15 +45,16 @@ UCCSD_LIH_SLICE_TIMES = [5.1 + _rz * 2, 2.85 + _rz * 2, 3.6 + _rz * 2,
                          3.4 + _rz * 2, 29.1 + _rz * 8, 39.25 + _rz * 8,
                          56.4 + _rz * 8, 25.55 + _rz * 8]
 
-# This pulse time for the full uccsd lih circuit. This time corresponds
+# The pulse time for the full uccsd lih circuit. This time corresponds
 # to that found by binary search at
 # /project/ftchong/qoc/thomas/uccsd_full_time/lih/
 # plus the maximum pulse time for an RZ gate times the number of RZ gates
 # in the circuit.
-UCCSD_LIH_FULL_TIME = 72.3 + _rz * 40
+UCCSD_LIH_FULL_TIME = 0 + _rz * 40
 
 
 ### UCCSD - BeH2 ###
+
 
 # A randomly generated theta vector to use for experimental consistency.
 UCCSD_BEH2_THETA = [1.910655366933038, 3.0380262019523134, 1.767835033803264,
